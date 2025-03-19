acb50e2a40e4e7a5eebddc5397dbe4022771fd95
rithwik777222 <rithwikk312@gmail.com>
2025-03-19 17:32:26 +0530

    project:attendence check

---
+++ attendence check.py
@@ -0,0 +1,6 @@
+attendence = int(input("enter the percentage of attendence:"))
+
+if attendence < 75:
+    print("you have failed to meet the attendence requirement to be promoted to the next grade")
+else:
+    print("your attendence meets the requirements to be promoted to the next grade")
