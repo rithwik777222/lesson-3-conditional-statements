d97ff43afcc80705019b82704cf894f6d80c31eb
rithwik777222 <rithwikk312@gmail.com>
2025-03-23 18:55:54 +0530

    exam eligibility

--- attendence check.py
+++ attendence check.py
@@ -4,14 +4,3 @@ if attendence < 75:
     print("you have failed to meet the attendence requirement to be promoted to the next grade")
 else:
     print("your attendence meets the requirements to be promoted to the next grade")
-
-import datetime
-#using now() to get current time
-current_time = datetime.datetime.now()
-
-print("time now at greenwich median is:",end="")
-print(current_time)
-
-#print calender of year 2025
-import calender
-print("\n", calender.calender(2025))
